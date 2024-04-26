7e2d5fe746344ee4fc53abc887e28ca49ab96240
Jose Ernesto Morales Ventura <jomorales.ventura@gmail.com>
2024-04-26 18:55:23 -0400

    ajustes

--- js/control.js
+++ js/control.js
@@ -1,3 +1,25 @@
-function test(){
-    console.log('TESTING...');
-}
+document.getElementById("juridico_form").addEventListener("submit", function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    
+    // Your JavaScript code to run after form submission
+    alert("Form submitted!");
+    // You can add more JavaScript code here
+    
+    // Now, you can submit the form programmatically if needed
+    // For example:
+    // this.submit();
+  });
+
+  document.getElementById("finca_form").addEventListener("submit", function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    
+    // Your JavaScript code to run after form submission
+    alert("Form submitted!");
+    // You can add more JavaScript code here
+    
+    // Now, you can submit the form programmatically if needed
+    // For example:
+    // this.submit();
+  });
